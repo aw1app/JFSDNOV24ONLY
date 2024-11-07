241ed713cef47b39acf9753b9d35bb84fdfa75d4
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-11-07 14:56:54 +0530

    updates

---
+++ MyFirstJavaProj/src/students/ForiegnStudent.java
@@ -0,0 +1,21 @@
+package students;
+
+import P1.Student;
+
+public class ForiegnStudent extends Student{
+	
+	public String nationality;
+	
+	void payFeesInDollars(float dollars){
+		
+	}
+	
+	public void displayDetails() {
+		
+		System.out.println(this.name);
+		
+		System.out.println(this.age);
+		
+	}
+
+}
