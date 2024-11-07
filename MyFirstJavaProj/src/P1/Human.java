a9dceb89bde813dd6838bfa8249748aaf768793b
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-11-07 17:01:56 +0530

    updates

---
+++ MyFirstJavaProj/src/P1/Human.java
@@ -0,0 +1,9 @@
+package P1;
+
+public interface Human {
+	int noOfHands =2;
+	
+	void speak(String words);
+	void walk();
+
+}
---
+++ MyFirstJavaProj/src/P1/Professor.java
@@ -0,0 +1,17 @@
+package P1;
+
+public class Professor implements Human{
+
+	@Override
+	public void speak(String words) {
+		System.out.println("Dear, ");
+		System.out.println(words);
+	}
+
+	@Override
+	public void walk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
